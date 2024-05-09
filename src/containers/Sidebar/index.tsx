8516710bc6232ab180c92acbeb8337085439c24f
Cássio Fellipe <cassio.fellipeff@gmail.com>
2024-05-09 06:02:47 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,20 @@
+import Avatar from "../../components/Avatar"
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Titulo"
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/cassiofellipe.png" />
-    <Titulo fontSize={20}>Cássio Fellipe</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Cássio Fellipe</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+      cassiofellipe
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>Desenvolvedor front-end</Descricao>
+      <BotaoTema>Trocar Tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
